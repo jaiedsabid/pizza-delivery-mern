2297d45544ffd7145ccd2c4e0b3b9dc989733b4e
Jaied Al Sabid <30983626+jaiedsabid@users.noreply.github.com>
2021-07-23 14:55:11 +0600

    Finished MongoDB database setup

---
+++ db.js
@@ -0,0 +1,10 @@
+const mongoose = require("mongoose");
+
+const DB_URL = `mongodb+srv://admin:@dmin@bug-tracker.qxllb.mongodb.net/flash_pizza?retryWrites=true&w=majority`;
+
+mongoose.set('returnOriginal', false);
+mongoose.connect(DB_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log("Database connected!"))
+    .catch((err) => `Error connecting database: ${err.message}`);
+
+module.exports = mongoose;
--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const app = express();
+const db = require("./db");
 
 app.use(express.json());
+
 
 
 app.get("/", (req, res) => {
