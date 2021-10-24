221fe7ca87bc33039f9e98ecea001f7e98b2b30f
Jaied Al Sabid <87969327+jaieds@users.noreply.github.com>
2021-10-24 18:47:36 +0600

    Updated the button functionality for decreasing items from the cart

--- client/src/redux/ActionCreators.js
+++ client/src/redux/ActionCreators.js
@@ -46,17 +46,20 @@ export const addToCart = (pizza, quantity, variant) => (dispatch, getState) => {
 
     if (quantity > 10) {
         alert("You can't add more than 10 Pizzas of the same type!");
-    } else if (quantity < 1) {
-        alert("You can't decrease quantity less than 1!");
+    } else if (quantity <= 0) {
+        // alert("You can't decrease quantity less than 1!");
+        dispatch({
+            type: ActionTypes.REMOVE_FROM_CART,
+            payload: cartItem
+        })
     } else {
         dispatch({
             type: ActionTypes.ADD_TO_CART,
             payload: cartItem
         })
-
-        const cartItems = getState().Cart.cartItems;
-        localStorage.setItem("cartItems", JSON.stringify(cartItems));
     }
+    const cartItems = getState().Cart.cartItems;
+    localStorage.setItem("cartItems", JSON.stringify(cartItems));
 }
 
 export const removeFromCart = (pizzaId, variant) => (dispatch, getState) =>{
