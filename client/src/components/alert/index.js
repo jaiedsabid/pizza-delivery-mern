21a90b29030856477b26c0ab6eba67c90d8482e9
Jaied Al Sabid <30983626+jaiedsabid@users.noreply.github.com>
2022-03-19 22:58:53 +0600

    fix: console error

--- client/src/components/alert/index.js
+++ client/src/components/alert/index.js
@@ -1,12 +1,22 @@
-import React, { memo, useState } from 'react';
+import React, { memo, useEffect, useRef, useState } from 'react';
 import { Alert as AlertBootstrap } from 'react-bootstrap';
 
 function Alert({ message, type, autoHideDuration = 3000, ...props }) {
     const [show, setShow] = useState(!!message);
+    const mounted = useRef();
 
     setTimeout(() => {
-        setShow(false);
+        if (mounted.current) {
+            setShow(false);
+        }
     }, autoHideDuration);
+
+    useEffect(() => {
+        mounted.current = true;
+        return () => {
+            mounted.current = false;
+        };
+    }, []);
 
     return show ? (
         <AlertBootstrap variant={type} {...props}>
